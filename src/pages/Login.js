2abb9b6f9305cb8e01af9654f07c6d5164aa7a7f
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-03-05 00:01:33 +0600

    login implemented

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
 import { useNavigate } from "react-router-dom";
 import loginImage from "../assets/login.svg";
+import { loginUser } from "../features/auth/authSlice";
 const Login = () => {
   const { register, handleSubmit, reset } = useForm();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = ({ email, password }) => {
+    dispatch(loginUser({ email, password }))
   };
 
   return (
